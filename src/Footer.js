039bc43030d877cba169623e16e12324cba51a02
swojcicki <slawek.wojcicki@gmail.com>
2020-05-31 12:14:17 +0200

    version on heroku

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import packageJson from './package-alias.json';
 import GitCommit from './_git_commit';
+import GitCommitHeroku from './_git_commit_heroku';
 
 function Footer() {
+
   return (
     <footer className="footer mt-auto py-3">
       <div className="container">
         <span className="text-muted">&copy; 2020 Sławomir Wójcicki |
-        version: {packageJson.version} build: {GitCommit.logMessage}</span>
+        version: {packageJson.version} build: {GitCommit.logMessage || GitCommitHeroku.logMessage}</span>
       </div>
     </footer>
     );
