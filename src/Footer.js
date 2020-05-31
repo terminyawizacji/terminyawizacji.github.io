95f50c0ecd895a34de8c919848c6552a836bf265
swojcicki <slawek.wojcicki@gmail.com>
2020-05-31 11:16:57 +0200

    version in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import packageJson from './package-alias.json';
+import GitCommit from './_git_commit';
 
 function Footer() {
   return (
     <footer className="footer mt-auto py-3">
       <div className="container">
-        <span className="text-muted">&copy; 2020 Sławomir Wójcicki</span>
+        <span className="text-muted">&copy; 2020 Sławomir Wójcicki |
+        version: {packageJson.version} build: {GitCommit.logMessage}</span>
       </div>
     </footer>
     );
