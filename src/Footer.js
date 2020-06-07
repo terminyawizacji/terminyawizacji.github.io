52c6d24eca6c841e5abf6d6f7cfe34837b3263de
swojcicki <slawek.wojcicki@gmail.com>
2020-06-07 16:50:27 +0200

    year footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,22 @@ import GitCommitHeroku from './_git_commit_heroku';
 
 function Footer() {
 
+  function getYear() {
+    let year = new Date().getFullYear();
+    if (year === 2020) {
+      return year;
+    }
+    return '2020 - ' + year;
+  }
+
   return (
     <footer className="footer mt-auto py-3">
       <div className="container">
-        <span className="text-muted">&copy; 2020 Sławomir Wójcicki |
+        <span className="text-muted">&copy; {getYear()} Sławomir Wójcicki |
         version: {packageJson.version} build: {GitCommit.logMessage || GitCommitHeroku.logMessage}</span>
       </div>
     </footer>
-    );
+  );
 }
 
 export default Footer;
